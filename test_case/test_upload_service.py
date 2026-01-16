e9a087043528c9de606c96521f5b82ad902dc806
hbc_thinkbook <419337244@qq.com>
2026-01-16 14:08:08 +0800

    feat 实现了百度网盘分片上传，并且清除缓存文件

--- test_case/test_upload_service.py
+++ test_case/test_upload_service.py
@@ -1,15 +1,35 @@
 from item_backup_item.service.upload_service import UploadService
+from dowhen import when
+
 
 
 def test_upload_service():
     params = {
-        'file_path':r'l:\QQDownload\bns_1.63.3865.2_setup_bin.7z.005',
-        'remote_path': '/apps/test_upload/',
+        # 'file_path':r'l:\QQDownload\bns_1.63.3865.2_setup_bin.7z.005',
+        'file_path': r'd:\压缩测试\20260115\解压密码_H_x123456789\normal_folder.zip',
         'chunk_size': 20*1024*1024,
         'rtype': 1
     }
     upload_service = UploadService(**params)
     upload_service.precreate().upload().create()
 
+def new_test_upload_service():
+    params = {
+        'file_path': r'd:\压缩测试\20260115\解压密码_H_x123456789\normal_folder.zip',
+        'chunk_size': 20*1024*1024,
+        'rtype': 1
+    }
+    params1 = {
+        'file_path': r'd:\压缩测试\20260115\解压密码_H_x123456789\normal_file.exe.zip',
+        'chunk_size': 20*1024*1024,
+        'rtype': 1
+    }
+    upload_service = UploadService()
+    result = upload_service.upload_file(**params)
+    print(f"result: {result}")
+    result1 = upload_service.upload_file(**params1)
+    print(f"result1: {result1}")
+
 if __name__ == "__main__":
     test_upload_service()
+    new_test_upload_service()
