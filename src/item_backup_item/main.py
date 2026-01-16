2b22ffe5b25bc5f6bfce6473c8b8e0ec2913ef8c
hbc_thinkbook <419337244@qq.com>
2026-01-16 17:38:45 +0800

    feat 跑通整个流程无误

--- src/item_backup_item/main.py
+++ src/item_backup_item/main.py
@@ -6,12 +6,12 @@ from .control import unzip_process
 from .control import unzip_hash_process
 from .control import upload_process
 def main():
-    # result = classify_process()
-    # result = hash_process()
-    # result = zip_process()
-    # result = zip_hash_process()
-    # result = unzip_process()
-    # result = unzip_hash_process()
+    result = classify_process()
+    result = hash_process()
+    result = zip_process()
+    result = zip_hash_process()
+    result = unzip_process()
+    result = unzip_hash_process()
     result = upload_process()   
     print(result)
 
