615aaf91f0a5359596d315580ba4b6ec2f9bb330
hbc_thinkbook <419337244@qq.com>
2026-01-16 18:04:24 +0800

    feat 差不多跑通删除流程 shutil无法删除非目录文件

--- src/item_backup_item/control/__init__.py
+++ src/item_backup_item/control/__init__.py
@@ -5,7 +5,7 @@ from .zip_hash import zip_hash_process
 from .unzip import unzip_process
 from .unzip_hash import unzip_hash_process
 from .upload import upload_process
-
+from .delete import delete_process
 __all__ = [
     "classify_process",
     "hash_process",
@@ -14,4 +14,5 @@ __all__ = [
     "unzip_process",
     "unzip_hash_process",   
     "upload_process",
+    "delete_process",
 ]
--- src/item_backup_item/main.py
+++ src/item_backup_item/main.py
@@ -5,14 +5,17 @@ from .control import zip_hash_process
 from .control import unzip_process
 from .control import unzip_hash_process
 from .control import upload_process
+from .control import delete_process
+
 def main():
-    result = classify_process()
-    result = hash_process()
-    result = zip_process()
-    result = zip_hash_process()
-    result = unzip_process()
-    result = unzip_hash_process()
-    result = upload_process()   
+    # result = classify_process()
+    # result = hash_process()
+    # result = zip_process()
+    # result = zip_hash_process()
+    # result = unzip_process()
+    # result = unzip_hash_process()
+    # result = upload_process()   
+    result = delete_process()
     print(result)
 
 
